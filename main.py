cceabcaeb490565d7d1e0ee815d52b91c7066a6e
Temi Babs <temi.ayo.babs@gmail.com>
2019-11-11 08:59:36 +0100

    * Code refactoring

--- main.py
+++ main.py
@@ -4,9 +4,21 @@ from flask import Flask, request, render_template
 from wtforms import Form
 
 from util.grammar import GrammarChecker
-
-
 app = Flask(__name__)
+
+
+def get_config():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--grammar_file', type=str, default='./grammar.cfg')
+    parser.add_argument('--dictionary', type=str, default='dictionary')
+    parser.add_argument('--input_file', type=str, default='sentences')
+    parser.add_argument('--web_mode', type=str, default=True)
+
+    return parser.parse_args()
+
+
+conf = get_config()
+grammar_checker = GrammarChecker(conf.grammar_file, conf.dictionary)
 
 
 @app.route('/', methods=['GET'])
@@ -36,20 +48,7 @@ def main(config):
             grammar_checker.check_grammar(sentence)
 
 
-def get_config():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--grammar_file', type=str, default='./grammar.cfg')
-    parser.add_argument('--dictionary', type=str, default='dictionary')
-    parser.add_argument('--input_file', type=str, default='sentences')
-    parser.add_argument('--web_mode', type=str, default=True)
-
-    return parser.parse_args()
-
-
 if __name__ == '__main__':
-    conf = get_config()
-    grammar_checker = GrammarChecker(conf.grammar_file, conf.dictionary)
-
     if not conf.web_mode:
         main(conf)
     else:
